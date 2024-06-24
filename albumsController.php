d05744e24f0f2da918aa764cf3da50ba5fea10ab
wailin193 <87842635+wailin193@users.noreply.github.com>
2024-06-24 23:04:41 +0630

    Add files via upload

---
+++ albumsController.php
@@ -0,0 +1,51 @@
+<?php 
+
+include_once '../model/albumsModel.php';
+
+class AlbumsController{
+
+    public $album;
+
+    function __construct()
+    {
+        $this->album = new AlbumsModel();
+    }
+
+    function albumsFromTrack(){
+        return $this->album->albumsFromTrack();
+    }
+
+    function getAlbums(){
+        return $this->album->getAlbums();
+    }
+
+    function addAlbums($name,$rdate,$photo){
+        return $this->album->addAlbums($name,$rdate,$photo);
+    }
+
+    function getName($id)
+    {
+        return $this->album->getName($id);
+    }
+
+    function editAlbum($id)
+    {
+        return $this->album->editalbum($id);
+    }
+
+    function updateAlbum($id,$name,$rdate,$photo)
+    {
+        return $this->album->updateAlbum($id,$name,$rdate,$photo);
+    }
+
+    public function deleteAlbum($id)
+    {
+        return $this->album->deleteAlbum($id);
+    }
+
+    public function detailTrack($album_id){
+        return $this->album->detailTrack($album_id);
+    }
+}
+
+?>
